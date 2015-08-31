5f1741ef8516ad3bd9c9ef72e3c8322bc4524906
Nik Samokhvalov <nik@samokhvalov.info>
2015-08-31 22:01:36 +0300

    Немного красоты от автоформатирования Шторма

--- install/index.php
+++ install/index.php
@@ -1,33 +1,34 @@
-<?
+<?php
+
 global $MESS;
 
 use Bitrix\Main\Localization\Loc;
 
 $PathInstall = str_replace("\\", "/", __FILE__);
-$PathInstall = substr($PathInstall, 0, strlen($PathInstall) - strlen("/index.php"));
-Loc::loadMessages($PathInstall . "/install.php");
-include($PathInstall . "/version.php");
+$PathInstall = substr($PathInstall, 0, strlen($PathInstall) - strlen('/index.php'));
+Loc::loadMessages($PathInstall . '/install.php');
+include($PathInstall . '/version.php');
 
-if (class_exists("digitalwand_admin_helper")) return;
+if (class_exists('digitalwand_admin_helper')) return;
 
-Class digitalwand_admin_helper extends CModule
+class digitalwand_admin_helper extends CModule
 {
-    var $MODULE_ID = "digitalwand.admin_helper";
+    var $MODULE_ID = 'digitalwand.admin_helper';
     var $MODULE_VERSION;
     var $MODULE_VERSION_DATE;
     var $MODULE_NAME;
     var $MODULE_DESCRIPTION;
-    var $MODULE_GROUP_RIGHTS = "Y";
+    var $MODULE_GROUP_RIGHTS = 'Y';
     var $MODULE_CSS;
-    var $PARTNER_NAME = "DigitalWand";
-    var $PARTNER_URI = "";
+    var $PARTNER_NAME = 'DigitalWand';
+    var $PARTNER_URI = '';
 
     function digitalwand_admin_helper()
     {
         $this->MODULE_VERSION = ADMIN_HELPER_VERSION;
         $this->MODULE_VERSION_DATE = ADMIN_HELPER_VERSION_DATE;
-        $this->MODULE_NAME = Loc::getMessage("ADMIN_HELPER_INSTALL_NAME");
-        $this->MODULE_DESCRIPTION = Loc::getMessage("ADMIN_HELPER_INSTALL_DESCRIPTION");
+        $this->MODULE_NAME = Loc::getMessage('ADMIN_HELPER_INSTALL_NAME');
+        $this->MODULE_DESCRIPTION = Loc::getMessage('ADMIN_HELPER_INSTALL_DESCRIPTION');
     }
 
     function DoInstall()
@@ -35,28 +36,20 @@ Class digitalwand_admin_helper extends CModule
         global $APPLICATION;
         RegisterModule($this->MODULE_ID);
         $this->InstallFiles();
-        $APPLICATION->IncludeAdminFile(
-            Loc::getMessage("ADMIN_HELPER_INSTALL_TITLE"),
-            __DIR__ . "/step.php"
-        );
+        $APPLICATION->IncludeAdminFile(Loc::getMessage('ADMIN_HELPER_INSTALL_TITLE'), __DIR__ . '/step.php');
     }
 
     function DoUninstall()
     {
         global $APPLICATION;
         UnRegisterModule($this->MODULE_ID);
-        $APPLICATION->IncludeAdminFile(
-            Loc::getMessage("ADMIN_HELPER_INSTALL_TITLE"),
-            __DIR__ . "/unstep.php"
-        );
+        $APPLICATION->IncludeAdminFile(Loc::getMessage('ADMIN_HELPER_INSTALL_TITLE'), __DIR__ . '/unstep.php');
     }
 
     function InstallFiles()
     {
-        CopyDirFiles(__DIR__ . "/admin", $_SERVER['DOCUMENT_ROOT'] . "/bitrix/admin");
+        CopyDirFiles(__DIR__ . '/admin', $_SERVER['DOCUMENT_ROOT'] . '/bitrix/admin');
 
         return true;
     }
 }
-
-?>
