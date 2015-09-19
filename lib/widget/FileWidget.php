ae9e7c804e39d1044f73d3f77317ae8ebe74188a
Dmitriy Baibuhtin <dmitriy.baibuhtin@ya.ru>
2015-09-19 16:22:59 +0300

    Попытка сделать FileWidget multiple

--- lib/widget/FileWidget.php
+++ lib/widget/FileWidget.php
@@ -8,16 +8,22 @@ Loc::loadMessages(__FILE__);
 
 /**
  * Для множественного поля в таблице должен быть столбец FILE_ID
+ * Настройки класса:
+ * <ul>
+ * <li><b>IMAGES</b> - bool ограничивать ли загрузку только изображениями</li>
+ * <li><b>DESCRIPTION</b> - bool нужно ли поле описания</li>
+ * <li><b>MULTIPLE</b> - bool является ли поле множественным</li>
+ * </ul>
  */
 class FileWidget extends HelperWidget
 {
-    /**
-     * Генерирует HTML для редактирования поля
-     * @return mixed
-     */
-    protected function genEditHTML()
-    {
-        return \CFileInput::Show($this->getEditInputName('_FILE'), ($this->getValue() > 0 ? $this->getValue() : 0),
+	/**
+	 * Генерирует HTML для редактирования поля
+	 * @return mixed
+	 */
+	protected function genEditHTML()
+	{
+		return \CFileInput::Show($this->getEditInputName('_FILE'), ($this->getValue() > 0 ? $this->getValue() : 0),
 			array(
 				"IMAGE" => "N",
 				"PATH" => "Y",
@@ -32,101 +38,142 @@ class FileWidget extends HelperWidget
 				'description' => false,
 			)
 		);
-    }
+	}
 
-    /**
-     * Генерирует HTML для поля в списке
-     * @see AdminListHelper::addRowCell();
-     * @param \CAdminListRow $row
-     * @param array $data - данные текущей строки
-     * @return mixed
-     */
-    public function genListHTML(&$row, $data)
-    {
-        $file = \CFile::GetPath($data[$this->code]);
-        $res = \CFile::GetByID($data[$this->code]);
-        $fileInfo = $res->Fetch();
+	protected function genMultipleEditHTML()
+	{
+		$style = $this->getSettings('STYLE');
+		$size = $this->getSettings('SIZE');
+		$uniqueId = $this->getEditInputHtmlId();
 
-        if (!$file)
-        {
-            $html = "";    
-        }
-        else
-        {
-            $html = '<a href="'.$file.'" >'.$fileInfo['FILE_NAME'].' ('.$fileInfo['FILE_DESCRIPTION'].')'.'</a>';
-        }
-        $row->AddViewField($this->code,$html);
+		ob_start();
+		?>
 
-    }
-    /**
-     * Генерирует HTML для поля фильтрации
-     * @see AdminListHelper::createFilterForm();
-     * @return mixed
-     */
-    public function genFilterHTML()
-    {
-        // TODO: Implement genFilterHTML() method.
-    }
+		<div id="<?= $uniqueId ?>-field-container" class="<?= $uniqueId ?>">
+		</div>
 
-    public function processEditAction()
-    {
-        if (isset($_REQUEST['FIELDS_del'][$this->code . '_FILE']) AND $_REQUEST['FIELDS_del'][$this->code . '_FILE'] == 'Y') {
-            \CFile::Delete(intval($this->data[$this->code]));
-            $this->data[$this->code] = 0;
-        }
-        else if (isset($_REQUEST['FIELDS']['IMAGE_ID_FILE']))
-        {
-            $name = $_FILES['FIELDS']['name'][$this->code.'_FILE'];
-            $path = $_REQUEST['FIELDS']['IMAGE_ID_FILE'];
-            $this->saveFile($name, $path);
-        }
-        else
-        {
-            $name = $_FILES['FIELDS']['name'][$this->code.'_FILE'];
-            $path = $_FILES['FIELDS']['tmp_name'][$this->code.'_FILE'];
-            $type = $_FILES['FIELDS']['type'][$this->code.'_FILE'];
-            $this->saveFile($name, $path, $type);
-        }
-    }
+		<script>
+			var multiple = new MultipleWidgetHelper(
+				'#<?= $uniqueId ?>-field-container',
+				'<input type="file" name="<?= $this->getCode()?>[][VALUE]" style="<?=$style?>" size="<?=$size?>">'
+			);
+			// TODO Добавление созданных полей
+			multiple.addField();
+		</script>
+		<?
+		return ob_get_clean();
+	}
 
-    protected function saveFile($name, $path, $type = false)
-    {
-        if (!$path)
-        {
-            return false;
-        }
-        
-        $fileInfo = \CFile::MakeFileArray(
-            $path,
-            $type
-        );
+	/**
+	 * Генерирует HTML для поля в списке
+	 * @see AdminListHelper::addRowCell();
+	 * @param \CAdminListRow $row
+	 * @param array $data - данные текущей строки
+	 * @return mixed
+	 */
+	public function genListHTML(&$row, $data)
+	{
+		$file = \CFile::GetPath($data[$this->code]);
+		$res = \CFile::GetByID($data[$this->code]);
+		$fileInfo = $res->Fetch();
 
+		if (!$file)
+		{
+			$html = "";
+		}
+		else
+		{
+			$html = '<a href="' . $file . '" >' . $fileInfo['FILE_NAME'] . ' (' . $fileInfo['FILE_DESCRIPTION'] . ')' . '</a>';
+		}
+		$row->AddViewField($this->code, $html);
+	}
 
-        if(!$fileInfo) return false;
-        
-        if (stripos($fileInfo['type'], "image") === false)
-        {
-            $this->addError('FILE_FIELD_TYPE_ERROR');
-            return false;
-        }
+	/**
+	 * Генерирует HTML для поля фильтрации
+	 * @see AdminListHelper::createFilterForm();
+	 * @return mixed
+	 */
+	public function genFilterHTML()
+	{
+		// TODO: Implement genFilterHTML() method.
+	}
 
-        $fileInfo["name"] = $name;
-        
-        /** @var AdminBaseHelper $model */
-        $helper = $this->helper;
-        $fileId = \CFile::SaveFile($fileInfo, $helper::$module);
+	public function processEditAction()
+	{
+		if ($this->getSettings('MULTIPLE'))
+		{
+			if (!empty($_FILES[$this->getCode()]))
+			{
+				foreach ($_FILES[$this->getCode()]['name'] as $key => $arFileName)
+				{
+					$fileName = $arFileName['VALUE'];
+					$fileTmpPath = $_FILES[$this->getCode()]['tmp_name'][$key]['VALUE'];
+					$fileId = $this->saveFile($fileName, $fileTmpPath);
+					$this->data['IMAGES'][] = ['VALUE' => $fileId];
+					// TODO Изображения не попадают в модель
+					// TODO В методе saveReferences в add в RelativeDataManager почему-то попадает int
+				}
+			}
+		}
+		else
+		{
+			if (isset($_REQUEST['FIELDS_del'][$this->code . '_FILE']) AND $_REQUEST['FIELDS_del'][$this->code . '_FILE'] == 'Y')
+			{
+				\CFile::Delete(intval($this->data[$this->code]));
+				$this->data[$this->code] = 0;
+			}
+			else if (isset($_REQUEST['FIELDS']['IMAGE_ID_FILE']))
+			{
+				$name = $_FILES['FIELDS']['name'][$this->code . '_FILE'];
+				$path = $_REQUEST['FIELDS']['IMAGE_ID_FILE'];
+				$this->saveFile($name, $path);
+			}
+			else
+			{
+				$name = $_FILES['FIELDS']['name'][$this->code . '_FILE'];
+				$path = $_FILES['FIELDS']['tmp_name'][$this->code . '_FILE'];
+				$type = $_FILES['FIELDS']['type'][$this->code . '_FILE'];
+				$this->saveFile($name, $path, $type);
+			}
+		}
+	}
 
-        $code = $this->code;
-        if(isset($this->data[$code])) {
-            \CFile::Delete($this->data[$code]);
-        }
+	protected function saveFile($name, $path, $type = false)
+	{
+		if (!$path)
+		{
+			return false;
+		}
 
-        if($this->getSettings('MULTIPLE')){
+		$fileInfo = \CFile::MakeFileArray(
+			$path,
+			$type
+		);
 
-        }
+		if (!$fileInfo) return false;
 
-        $this->data[$code] = $fileId;
-        return true;
-    }
+		if (stripos($fileInfo['type'], "image") === false)
+		{
+			$this->addError('FILE_FIELD_TYPE_ERROR');
+
+			return false;
+		}
+
+		$fileInfo["name"] = $name;
+
+		/** @var AdminBaseHelper $model */
+		$helper = $this->helper;
+		$fileId = \CFile::SaveFile($fileInfo, $helper::$module);
+
+		$code = $this->code;
+		if (isset($this->data[$code]))
+		{
+			\CFile::Delete($this->data[$code]);
+		}
+
+		$this->data[$code] = $fileId;
+
+		return $fileId;
+	}
 
 }
