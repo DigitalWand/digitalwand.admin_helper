ccb063b263c16fcb601b2705163c20518b540026
Artem Yarygin <artx19@yandex.ru>
2015-10-08 17:06:08 +0400

    автоматическая регистрация класса админского интерфейса

--- admin/route.php
+++ admin/route.php
@@ -32,6 +32,29 @@ if (!$module OR !$view OR !Loader::IncludeModule($module)) {
     include $_SERVER['DOCUMENT_ROOT'] . BX_ROOT . '/admin/404.php';
 }
 
+/**
+ * Пытаемся определить имя класса админского интерфейса
+ */
+$interfaceNameParts = explode('.', $module);
+$helperNameParts = explode('_', $view);
+$entityName = array_shift($helperNameParts);
+$interfaceNameParts[]=$entityName;
+$interfaceNameParts[]= $entityName.'AdminInterface';
+foreach($interfaceNameParts as $i => $v)
+{
+    $interfaceNameParts[$i] = ucfirst($v);
+}
+$interfaceNameClass = implode('\\',$interfaceNameParts);
+
+/**
+ * Регистрируем класс интерфейса если он существует
+ */
+if(class_exists($interfaceNameClass))
+{
+    $interfaceNameClass::register();
+}
+
+
 list($helper, $interface) = AdminBaseHelper::getGlobalInterfaceSettings($module, $view);
 
 if (!$helper OR !$interface) {
