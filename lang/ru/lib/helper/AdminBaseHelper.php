aa30e5d73a1836e84f45e5b999801c43034c791c
Nik Samokhvalov <nik@samokhvalov.info>
2015-11-16 18:57:24 +0300

    Small fixes

--- lang/ru/lib/helper/AdminBaseHelper.php
+++ lang/ru/lib/helper/AdminBaseHelper.php
@@ -1,4 +1,4 @@
 <?php
 
-$MESS['DIGITALWAND_ADMIN_HELPER_GETMODEL_EXCEPTION'] = "Попыка обращение к несуществующему HL-инфоблоку #CLASS#";
-$MESS['DIGITALWAND_ADMIN_HELPER_ACCESS_FORBIDDEN'] = "Доступ запрещен";
+$MESS['DIGITALWAND_ADMIN_HELPER_GETMODEL_EXCEPTION'] = 'Попыка обращение к несуществующему HL-инфоблоку #CLASS#';
+$MESS['DIGITALWAND_ADMIN_HELPER_ACCESS_FORBIDDEN'] = 'Доступ запрещен';
