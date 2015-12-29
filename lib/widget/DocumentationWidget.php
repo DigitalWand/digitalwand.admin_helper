b518a4852772ad843c97482d61b8a3ee981fa752
Artem Yarygin <artx19@yandex.ru>
2015-12-29 17:33:39 +0400

    высплывающее окно с текстом документации через popup_core.js, кнопка через CAdminContextMenu

--- lib/widget/DocumentationWidget.php
+++ lib/widget/DocumentationWidget.php
@@ -22,17 +22,35 @@ class DocumentationWidget
     {
         $this->buttonText = $params['TEXT'];
         $this->popupText = $params['POPUP_TEXT'];
+
         return $this;
     }
 
     public function show()
     {
-        if(!empty($this->buttonText) && !empty($this->popupText)):?>
-            <div style="float:right">
-                <?=$this->buttonText?>
-            </div>
+        if (!empty($this->buttonText) && !empty($this->popupText)):
+            \CUtil::InitJSCore(array('window'));
+            $aContext = array(
+                array(
+                    "TEXT" => $this->buttonText,
+                    "ONCLICK" => 'DocDialog.Show();',
+                    "ICON" => "btn_default",
+                ),
+            );
+            $oMenu = new \CAdminContextMenu($aContext); ?>
+            <script type="text/javascript">
+                DocDialog = new BX.CDialog({
+                    title: "<?=$this->buttonText?>",
+                    content: '<?=$this->popupText?>',
+                    icon: 'head-block',
+                    resizable: true,
+                    draggable: true,
+                    buttons: [BX.CDialog.btnClose]
+                });
+            </script>
+            <div style="float:right;padding-top:2px;"><? $oMenu->Show(); ?></div>
             <div style="clear:both"></div>
-            <style type="text/css">#adm-title{float:left}</style>
+            <style type="text/css">#adm-title { float: left }</style>
         <?endif;
     }
 
