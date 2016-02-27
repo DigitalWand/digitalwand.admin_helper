0112da5458f36f27b8ba6dec3ccea8ba4d7d156d
ASGAlex <asgalex@gmail.com>
2016-02-28 00:59:37 +0300

    fix #22: try different paths, 404 if all failed

--- install/admin/admin_helper_route.php
+++ install/admin/admin_helper_route.php
@@ -1,3 +1,8 @@
 <?
-require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/digitalwand.admin_helper/admin/route.php");
+$bitrixPath = $_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/digitalwand.admin_helper/admin/route.php";
+$localPath = $_SERVER["DOCUMENT_ROOT"] . "/local/modules/digitalwand.admin_helper/admin/route.php";
+
+if(!@include_once($bitrixPath) AND !@include_once($localPath) ){
+    include $_SERVER['DOCUMENT_ROOT'] . '/bitrix/admin/404.php';
+}
 ?>
