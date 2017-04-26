5ae78d688cfe0b66d2a10e9bb5f65d1870cf1752
Marat Shamshutdinov <marat@citrus-soft.ru>
2017-04-26 14:40:13 +0300

    fix

--- lib/helper/AdminEditHelper.php
+++ lib/helper/AdminEditHelper.php
@@ -150,10 +150,10 @@ abstract class AdminEditHelper extends AdminBaseHelper
 			}
 
 			if (isset($url)) {
-				if (defined('BX_PUBLIC_MODE') && BX_PUBLIC_MODE === 1 && $this->getErrors())
+				if (defined('BX_PUBLIC_MODE') && BX_PUBLIC_MODE === 1 && ($errors = $this->getErrors()))
 				{
 					ob_end_clean();
-					$jsMessage = \CUtil::JSEscape(implode("\n", $this->getErrors()));
+					$jsMessage = \CUtil::JSEscape(implode("\n", $errors));
 					echo '<script>top.BX.WindowManager.Get().ShowError("' . $jsMessage . '");</script>';
 					die();
 				}
