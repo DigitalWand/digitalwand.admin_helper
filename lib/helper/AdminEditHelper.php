bef16d37b22d16659449f8d2c37ed64f8ce22d9c
ASGAlex <asgalex@gmail.com>
2017-12-17 00:51:04 +0300

    #20 refactoring

--- lib/helper/AdminEditHelper.php
+++ lib/helper/AdminEditHelper.php
@@ -447,10 +447,7 @@ abstract class AdminEditHelper extends AdminBaseHelper
 			$id = $this->getPk();
 
 			if ($id) {
-				/** @var DataManager $className */
-				$className = static::getModel();
-				// Если имеется primary key, то модель уже существующая, пытаемся найти ее в БД
-				$existing = $className::getById($id)->fetch();
+				$existing = $this->loadElement();
 			}
 
 			if ($existing) {
